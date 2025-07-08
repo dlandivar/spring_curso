f6e5fec142bb1f49c83244a49d379cd64bb00023
dlandivar <d_landivar@hotmail.com>
2025-07-08 18:25:21 -0500

    edit delete

--- src/main/java/bce/ec/demo/controllers/UserController.java
+++ src/main/java/bce/ec/demo/controllers/UserController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,20 +34,24 @@ public class UserController {
         return userService.create(user);
     }
 
+//@PostMapping("/add-users")
+//public Resp 
+
     @GetMapping("/{userId}")
     public User getUser(@PathVariable("userId") int userId) {
         return userService.getUserById(userId);
     }
     
-@GetMapping("/update/{userId}")
-    public User updateUser(@PathVariable("userId") User userId) {
-        return userService.updateById(userId);
+@PutMapping("/update/{userId}")
+    public User updateUser(@PathVariable("userId") int userId,@RequestBody User user) {
+        user.setId(userId); // Ensure the ID is set for the update
+        return userService.updateById(user);
     }
 
-  @GetMapping("/delete/{userId}")
-    public User deleteUser(@PathVariable("userId") int userId) {
-        return userService.getUserById(userId);
+  @DeleteMapping("/delete/{userId}")
+    public void deleteUser(@PathVariable("userId") int userId) {
+         userService.deleteById(userId);
     }
 
-    
+    //nocontent
 }
